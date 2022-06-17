490ca6075b38685d967c8b2c97d673f15e00d81b
Jobsidney <jobsidney67@gmail.com>
2022-06-17 14:25:35 +0300

    First commit

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //n 
+for (let i = 0; i < array.length; i++) {
+  //nested loop n*2
+  let firstComparison = target-array[i];
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] === firstComparison  && i!==j){
+      return true;
+    }
+  }
+  return false;
 }
+}
+console.log(hasTargetSum([22, 19, 4, 6, 30],25));
+
 
 /* 
   Write the Big O time complexity of your function here
+
+  time complexity:0(n^2)
+  space complexity: 0(n)
 */
 
 /* 
   Add your pseudocode here
+  for loop iteration to array given
+  find first comparison by substracting it from target
+
 */
 
 /*
   Add written explanation of your solution here
+  take one argument from array, subtract it from target then compare it 
+  to the other values in array. if it matches return true if not
+  go to another value and repeat till you run out of 
+  values in array.
 */
 
 // You can run `node index.js` to view these console logs
